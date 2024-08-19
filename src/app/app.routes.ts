4122c43838735c64e6b296dacdbe96c35ab6bb4f
LerouvilloisValentin <lerouvillois.valentin61@hotmail.fr>
2024-08-19 16:46:36 +0200

    add home forum page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { HomeForumComponent } from './home-forum/home-forum.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'books', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'forum', component: HomeForumComponent },
 ];
