eece34499e2614c19b7928e645399ba1ea80192a
Sullyvan Boulanger <sullyvanboulanger@gmail.com>
2024-08-26 11:33:46 +0200

    feat: Implement reintegrate and refresh

--- src/app/user-management/user-management.component.ts
+++ src/app/user-management/user-management.component.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { UserSuspendComponent } from '../user-suspend/user-suspend.component';
 import { User } from '../user.model';
+import { SubscribeManagementComponent } from '../subscribe-management/subscribe-management.component';
 
 @Component({
   selector: 'app-user-management',
@@ -12,22 +13,22 @@ import { User } from '../user.model';
   templateUrl: './user-management.component.html',
   styleUrl: './user-management.component.css',
 })
-export class UserManagementComponent implements OnInit {
+export class UserManagementComponent
+  extends SubscribeManagementComponent
+  implements OnInit
+{
   users: User[] = [];
-  isModalOpen: boolean = false;
-  suspensionDays: number = 0;
-  constructor(private userManagementService: UserManagementService) {}
+
+  constructor(private userManagementService: UserManagementService) {
+    super();
+  }
 
   ngOnInit() {
     this.loadUsers();
   }
 
-  isOpen() {
-    this.isModalOpen = !this.isModalOpen;
-  }
-
   loadUsers() {
-    this.userManagementService.getUser().subscribe(
+    const sub = this.userManagementService.getUser().subscribe(
       (value: any[]) => {
         this.users = value;
         console.log(value);
@@ -36,27 +37,42 @@ export class UserManagementComponent implements OnInit {
         console.error('Erreur lors du chargement des utilisateurs', error);
       }
     );
+
+    this.addSubscription(sub);
   }
 
   suspendUser({ id, days }: { id: number; days: number }) {
-    // Vérifiez que this.suspensionDays est bien défini
-    if (this.suspensionDays === undefined || this.suspensionDays === null) {
+    if (days === undefined || days === null) {
       console.error("Le nombre de jours de suspension n'est pas défini");
       return;
     }
 
-    this.userManagementService
-      .suspendUser(id, days)
-      .subscribe(
-        (value: any) => {
-          console.log('Utilisateur suspendu', value);
-        },
-        (error: any) => {
-          console.error("Erreur lors de la suspension de l'utilisateur", error);
-        }
-      );
+    const sub = this.userManagementService.suspendUser(id, days).subscribe(
+      (value: any) => {
+        console.log('Utilisateur suspendu', value);
+        this.loadUsers();
+      },
+      (error: any) => {
+        console.error("Erreur lors de la suspension de l'utilisateur", error);
+      }
+    );
 
-    // Fermez le modal
-    this.isOpen();
+    this.addSubscription(sub);
+  }
+
+  reintegrateUser(id: number) {
+    console.log(id);
+
+    const sub = this.userManagementService.reintegrateUser(id).subscribe(
+      (value: any) => {
+        console.log('Utilisateur plus suspendu', value);
+        this.loadUsers();
+      },
+      (error: any) => {
+        console.error("Erreur lors de la suspension de l'utilisateur", error);
+      }
+    );
+
+    this.addSubscription(sub);
   }
 }
--- src/app/user-suspend/user-suspend.component.ts
+++ src/app/user-suspend/user-suspend.component.ts
@@ -1,17 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { User } from '../user.model';
 import { FormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-suspend',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, DatePipe],
   templateUrl: './user-suspend.component.html',
   styleUrl: './user-suspend.component.css',
 })
 export class UserSuspendComponent {
   @Input() user?: User;
   @Output() onSuspend = new EventEmitter<{ id: number; days: number }>();
+  @Output() onReintegrate = new EventEmitter<number>();
   isModalOpen: boolean = false;
   suspensionDays: number = 0;
 
@@ -22,6 +24,13 @@ export class UserSuspendComponent {
   suspend() {
     if (this.user) {
       this.onSuspend.emit({ id: this.user.id, days: this.suspensionDays });
+    }
+    this.isOpen();
+  }
+
+  reintegrate() {
+    if (this.user) {
+      this.onReintegrate.emit(this.user.id);
     }
   }
 }
