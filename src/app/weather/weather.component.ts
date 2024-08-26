4015ee95f91601c39c695051c725fa22678a60d8
LucasDRT86 <lucasdorat2@gmail.com>
2024-08-26 14:24:37 +0200

    Weather restylize

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -3,6 +3,17 @@ import { WeatherService } from '../weather.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faSun,
+  faCloud,
+  faCloudRain,
+  faBolt,
+  faSnowflake,
+  faSmog,
+  faCloudShowersHeavy,
+  faCloudMeatball,
+  faCloudSun,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-weather',
@@ -14,6 +25,16 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 export class WeatherComponent implements OnInit {
   weatherData: any;
   research: string = '';
+
+  faSun = faSun;
+  faCloud = faCloud;
+  faCloudRain = faCloudRain;
+  faBolt = faBolt;
+  faSnowflake = faSnowflake;
+  faSmog = faSmog;
+  faCloudShowersHeavy = faCloudShowersHeavy;
+  faCloudMeatball = faCloudMeatball;
+  faCloudSun = faCloudSun;
 
   constructor(private weatherService: WeatherService) {}
 
@@ -27,6 +48,26 @@ export class WeatherComponent implements OnInit {
           console.error('Error fetching weather data', error);
         }
       );
+    }
+  }
+
+  getWeatherIcon(weatherType: string): any {
+    if (/ensoleillé|sunny/i.test(weatherType)) {
+      return this.faSun; // Icône pour Ensoleillé
+    } else if (/nuageux|cloudy|overcast/i.test(weatherType)) {
+      return this.faCloudSun; // Icône pour Nuageux
+    } else if (/pluie|rain|drizzle/i.test(weatherType)) {
+      return this.faCloudRain; // Icône pour Pluie
+    } else if (/orage|thunderstorm/i.test(weatherType)) {
+      return this.faBolt; // Icône pour Orage
+    } else if (/neige|snow/i.test(weatherType)) {
+      return this.faSnowflake; // Icône pour Neige
+    } else if (/brouillard|fog/i.test(weatherType)) {
+      return this.faSmog; // Icône pour Brouillard
+    } else if (/grêle|hail/i.test(weatherType)) {
+      return this.faCloudMeatball; // Icône pour Grêle
+    } else {
+      return this.faCloud; // Icône par défaut
     }
   }
 }
